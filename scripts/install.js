12f83be15e085b7114772e81f8e425800767149b
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2024-02-17 11:24:45 +0000

    Fix mac os after install

--- scripts/install.js
+++ scripts/install.js
@@ -1,8 +1,14 @@
 const { execSync } = require('child_process');
+const os = require('os');
 
 async function run() {
+  const arch = `${os.platform()}-${os.arch().toLowerCase()}`;
   try {
     await execSync('node-gyp rebuild');
+    if (arch === 'darwin-arm64') {
+      await execSync('install_name_tool -change ./libGLESv2.dylib @rpath/libGLESv2.dylib build/Release/nodejs_gl_binding_darwin_arm64.node');
+      await execSync('install_name_tool -change ./libEGL.dylib @rpath/libEGL.dylib build/Release/nodejs_gl_binding_darwin_arm64.node');
+    }
   } catch (e) {
     console.error(e);
   }
