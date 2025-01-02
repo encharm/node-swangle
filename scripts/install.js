acfd591ec733075ad0ac552915545e35c70060aa
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2025-01-02 16:37:21 +0100

    Fix build

--- scripts/install.js
+++ scripts/install.js
@@ -1,17 +1,10 @@
 const { execSync } = require('child_process');
 const os = require('os');
 
-async function run() {
-  const arch = `${os.platform()}-${os.arch().toLowerCase()}`;
-  try {
-    await execSync('node-gyp rebuild');
-    if (arch === 'darwin-arm64') {
-      await execSync('install_name_tool -change ./libGLESv2.dylib @rpath/libGLESv2.dylib build/Release/nodejs_gl_binding_darwin_arm64.node');
-      await execSync('install_name_tool -change ./libEGL.dylib @rpath/libEGL.dylib build/Release/nodejs_gl_binding_darwin_arm64.node');
-    }
-  } catch (e) {
-    console.error(e);
-  }
-}
+const arch = `${os.platform()}-${os.arch().toLowerCase()}`;
+console.log('install ', arch)
 
-run();
+if (arch === 'darwin-arm64') {
+  execSync('install_name_tool -change ./libGLESv2.dylib @rpath/libGLESv2.dylib build/Release/nodejs_gl_binding_darwin_arm64.node');
+  execSync('install_name_tool -change ./libEGL.dylib @rpath/libEGL.dylib build/Release/nodejs_gl_binding_darwin_arm64.node');
+}
